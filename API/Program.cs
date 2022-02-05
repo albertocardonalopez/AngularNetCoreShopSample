3e37a837683281076620d5940c69d426403cf35a
Alberto Cardona <cardona.lopez.alberto@gmail.com>
2022-02-05 21:19:43 +0100

    add base specification class

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
---
+++ Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public Expression<Func<T, bool>> Criteria => throw new NotImplementedException();
+
+        public List<Expression<Func<T, object>>> Includes => throw new NotImplementedException();
+    }
+}
