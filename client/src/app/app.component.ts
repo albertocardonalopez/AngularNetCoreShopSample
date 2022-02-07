290120b21d18bee81d9a20e821b2c6123ec47e9b
Alberto Cardona <cardona.lopez.alberto@gmail.com>
2022-02-07 07:57:14 +0100

    add folder structure and components for core, shared, shop

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+import { IPagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
