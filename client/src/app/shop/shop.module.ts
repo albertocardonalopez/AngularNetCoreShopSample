4eb693a14d3be39ec8fd956e0320d29a744b1e2e
Alberto Cardona <cardona.lopez.alberto@gmail.com>
2022-02-26 02:47:22 +0100

    lazy-load angular shop module

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,7 +4,7 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 
@@ -17,10 +17,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
-  ],
-  exports: [
-    ShopComponent
+    ShopRoutingModule
   ]
 })
 export class ShopModule { }
