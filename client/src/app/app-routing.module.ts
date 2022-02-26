f28fca1403553d902c3542e37dc4f5ab0e03de7f
Alberto Cardona <cardona.lopez.alberto@gmail.com>
2022-02-26 03:24:42 +0100

    add test-error component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'test-error', component: TestErrorComponent},
   {path:'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
   {path:'**', redirectTo: '', pathMatch: 'full'}
 ];
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    TestErrorComponent
   ],
   imports: [
     CommonModule,
