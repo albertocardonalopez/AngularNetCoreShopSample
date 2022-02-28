6e2f050479310e18913a22f7b5a7f6e3555dde25
Alberto Cardona <cardona.lopez.alberto@gmail.com>
2022-02-28 07:45:31 +0100

    add loading spinner and adapt ui accordingly

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -15,7 +15,9 @@ export class ProductDetailsComponent implements OnInit {
   constructor(
     private shopService: ShopService,
     private activatedRoute: ActivatedRoute,
-    private breadcrumbService: BreadcrumbService) { }
+    private breadcrumbService: BreadcrumbService) {
+      this.breadcrumbService.set('@productDetails', ' ');
+    }
 
   ngOnInit(): void {
     this.loadProduct();
