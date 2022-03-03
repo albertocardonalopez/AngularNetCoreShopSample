30aeada8aadc462a683a262b8d0bf0e8709ed4fd
Alberto Cardona <cardona.lopez.alberto@gmail.com>
2022-03-03 11:36:47 +0100

    hook up product-detail to the basket

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from 'src/app/basket/basket.service';
+import { IBasketItem } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-product-details',
@@ -11,11 +13,14 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct = {} as IProduct;
+  existingProduct: IBasketItem | undefined;
+  quantity: number = 1;
 
   constructor(
     private shopService: ShopService,
     private activatedRoute: ActivatedRoute,
-    private breadcrumbService: BreadcrumbService) {
+    private breadcrumbService: BreadcrumbService,
+    private basketService: BasketService) {
       this.breadcrumbService.set('@productDetails', ' ');
     }
 
@@ -23,14 +28,51 @@ export class ProductDetailsComponent implements OnInit {
     this.loadProduct();
   }
 
+  addItemToBasket() {
+    if (!this.existingProduct) {
+      this.basketService.addItemToBasket(this.product, this.quantity);
+    } else {
+      this.basketService.addItemToBasket(this.product, this.quantity - this.existingProduct.quantity);
+    }
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
   loadProduct() {
     const id = this.activatedRoute.snapshot.paramMap.get('id') || 0 as number;
     this.shopService.getProduct(+id).subscribe(product => {
       this.product = product;
       this.breadcrumbService.set('@productDetails', product.name);
+      const basket = this.basketService.getCurrentBasketValue();
+      var productMaybe = this.getExistingItemFromBasket();
+      if (productMaybe) {
+        this.quantity = productMaybe.quantity;
+        this.existingProduct = productMaybe;
+      }
     }, error => {
       console.log(error);
     });
+  }
+
+  private getExistingItemFromBasket(): IBasketItem | undefined {
+    const basket = this.basketService.getCurrentBasketValue();
+
+    if (basket) {
+      if (basket.items.some(x => x.id === this.product.id)) {
+        const existingProduct = basket.items.find(x => x.id === this.product.id);
+        return existingProduct;
+      }
+    }
+
+    return undefined;
   }
 
 }
