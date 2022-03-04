2c0cb6c2780469fd9584938e5edafc7bc7e12223
Alberto Cardona <cardona.lopez.alberto@gmail.com>
2022-03-04 10:34:57 +0100

    create login form and display nav-bar accordingly

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
 
-  constructor() { }
+  constructor(
+    private accountService: AccountService
+  ) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = new FormGroup({
+      email: new  FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit() {
+    this.accountService.login(this.loginForm.value).subscribe(() => {
+      console.log('user logged in');
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
