4917e2e6424b041424e755122ddd3ceb122fbe08
Alberto Cardona <cardona.lopez.alberto@gmail.com>
2022-03-04 08:57:00 +0100

    add swagger config for authorization

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Extensions
+﻿using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
 {
     public static class SwaggerServiceExtensions
     {
@@ -6,7 +8,30 @@
         {
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Angular Net Core Shop Sample API", Version = "v1" });
+
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement { 
+                    { securitySchema, new [] { "Bearer" } } };
+                c.AddSecurityRequirement(securityRequirement);
+
+            });
 
             return services;
         }
