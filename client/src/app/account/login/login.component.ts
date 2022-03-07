561a72cb09b336e483bbd7163c40ab129d6d2197
Alberto Cardona <cardona.lopez.alberto@gmail.com>
2022-03-07 10:25:14 +0100

    add generic text-input for validation

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent implements OnInit {
 
   createLoginForm() {
     this.loginForm = new FormGroup({
-      email: new  FormControl('', Validators.required),
+      email: new  FormControl('', [Validators.required, Validators.email, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
       password: new FormControl('', Validators.required)
     });
   }
