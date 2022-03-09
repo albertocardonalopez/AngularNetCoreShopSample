3ef39eb0eb9ff2fb2e90b6c1cd81ae3f9188bbbe
Alberto Cardona <cardona.lopez.alberto@gmail.com>
2022-03-09 11:50:50 +0100

    implement unit of work and refactor

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -13,6 +13,7 @@ namespace API.Extensions
         {
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<IBasketRepository, BasketRepository>();   
             services.AddScoped<IProductRepository, ProductRepository>();
--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -11,20 +11,14 @@ namespace Infrastructure.Services
 {
     public class OrderService : IOrderService
     {
-        private readonly IGenericRepository<Order> _orderRepository;
-        private readonly IGenericRepository<DeliveryMethod> _deliveryMethodRepository;
-        private readonly IGenericRepository<Product> _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IBasketRepository _basketRepository;
 
         public OrderService(
-            IGenericRepository<Order> orderRepository,
-            IGenericRepository<DeliveryMethod> deliveryMethodRepository,
-            IGenericRepository<Product> productRepository,
+            IUnitOfWork unitOfWork,
             IBasketRepository basketRepository)
         {
-            _orderRepository = orderRepository;
-            _deliveryMethodRepository = deliveryMethodRepository;
-            _productRepository = productRepository;
+            _unitOfWork = unitOfWork;
             _basketRepository = basketRepository;
         }
 
@@ -35,17 +29,24 @@ namespace Infrastructure.Services
             var items = new List<OrderItem>();
             foreach (var item in basket.Items)
             {
-                var productItem = await _productRepository.GetByIdAsync(item.Id);
+                var productItem = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
                 var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name, productItem.PictureUrl);
                 var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
                 items.Add(orderItem);
             }
 
-            var deliveryMethod = await _deliveryMethodRepository.GetByIdAsync(deliveryMethodId);
+            var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync(deliveryMethodId);
 
             var subtotal = items.Sum(x => x.Price * x.Quantity);
 
             var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+            _unitOfWork.Repository<Order>().Add(order);
+
+            var result = await _unitOfWork.Complete();
+
+            if (result <= 0) return null;
+
+            await _basketRepository.DeleteBasketAsync(basketId);
 
             return order;
         }
